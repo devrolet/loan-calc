68186bd267eaf02b6a1a4815ab0d7934e3df8e74
Chet Hill <chetdhill@gmail.com>
2019-07-27 18:54:53 -0400

    Create Methods and handle Events

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,25 +2,46 @@ import React, { Component } from 'react';
 
 class Form extends Component {
     state = {  }
+
+    // Create method in React (Preferred Way) normal way works as well
+    handleChange = () => {
+        console.log('Form Validated');
+    }
+
+    handleSubmit = () => {
+        console.log('Form Submitted');
+    }
+
     render() { 
         return ( 
            <form>
                <div>
                 <label>Amount</label>
-                <input name="amount" className="u-full-width" type="number" placeholder="Eg: 3000" />
+                <input 
+                    onChange={this.handleChange}
+                    name="amount" 
+                    className="u-full-width" 
+                    type="number" 
+                    placeholder="Eg: 3000" />
                </div>
                <div>
                 <label>Months to Repay</label>
-                <select name="months" className="u-full-width">
-                    <option value="">Select</option>
-                    <option value="3">3 Months</option>
-                    <option value="6">6 Months</option>
-                    <option value="12">12 Months</option>
-                    <option value="24">24 Months</option>
+                <select 
+                    onChange={this.handleChange} 
+                    name="months" 
+                    className="u-full-width">
+                        <option value="">Select</option>
+                        <option value="3">3 Months</option>
+                        <option value="6">6 Months</option>
+                        <option value="12">12 Months</option>
+                        <option value="24">24 Months</option>
                 </select>
                </div>
                <div>
-                   <input type="submit" value="Calculate" className="u-full-width button-primary" />
+                   <input 
+                    onClick={this.handleSubmit} 
+                    type="submit" value="Calculate" 
+                    className="u-full-width button-primary" />
                </div>
            </form>
          );

1ef272ef63cc47701333ecb1764f5c621466e2e8
Chet Hill <chetdhill@gmail.com>
2019-07-28 17:20:58 -0400

    Add Spinner Component w conditional

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Form from './components/Form.js';
 import {calculateTotal} from './helper';
 import Result from './components/Result.js';
 import Message from './components/Message.js';
+import Spinner from './components/Spinner.js';
 
 import './normalize.css';
 import './skeleton.css';
@@ -13,28 +14,41 @@ class App extends Component {
   state = {
     total: '',
     amount: '',
-    term: ''
+    term: '',
+    loading: false
   }
 
   loanInformation = (amount, term) => {
 
     const total = calculateTotal(amount, term);
 
+    // this.setState({
+    
+    // })
     this.setState({
-      amount,
-      total,
-      term
+      loading: true
+    }, () => {
+      setTimeout(() => {
+        this.setState({
+          amount,
+          total,
+          term,
+          loading: false
+        })
+      }, 3000);
     })
   }
   render() {
     // Destructure the state
-    const {term, amount, total} = this.state;
+    const {term, amount, total, loading} = this.state;
 
     // Conditionally Render a Component
 
     let component;
-    if(total === '') {
+    if(total === '' && !loading) {
       component = <Message />
+    } else if(loading) {
+      component = <Spinner />
     } else {
       component = <Result 
                     total={total}

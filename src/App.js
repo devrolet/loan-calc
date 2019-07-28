d7b50bae64caba92adf1f2b7f94c1499eec1ffd3
Chet Hill <chetdhill@gmail.com>
2019-07-28 16:42:06 -0400

    Conditional Render of Components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Form from './components/Form.js';
 import {calculateTotal} from './helper';
 import Result from './components/Result.js';
+import Message from './components/Message.js';
 
 import './normalize.css';
 import './skeleton.css';
@@ -26,7 +27,21 @@ class App extends Component {
     })
   }
   render() {
+    // Destructure the state
     const {term, amount, total} = this.state;
+
+    // Conditionally Render a Component
+
+    let component;
+    if(total === '') {
+      component = <Message />
+    } else {
+      component = <Result 
+                    total={total}
+                    amount={amount}
+                    term={term}
+                  />
+    }
     return (
       <Fragment>
         <div className="App">
@@ -36,11 +51,7 @@ class App extends Component {
               loanInformation={this.loanInformation}
             />
             <div className="messages">
-              <Result 
-                total={total}
-                amount={amount}
-                term={term}
-              />
+              {component}
             </div>
           </div>
         </div>
---
+++ src/components/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Message = () => (
+    <p>Add an Amount and the Term to Repay</p>
+)
+ 
+export default Message;
